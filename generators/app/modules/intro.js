09e60a8d9f8228c12db541fee1bd8926db260a5c
Gamel James <gamel@meezyart.com>
2018-06-25 23:32:33 -0700

    Update generator name

--- generators/app/modules/intro.js
+++ generators/app/modules/intro.js
@@ -6,9 +6,9 @@ const chalk = require('chalk');
 
 module.exports = function intro() {
   const message = chalk.yellow('\n Its Fetch') +
-  chalk.green('\n  _____                     _   _') +
-  chalk.green('\n | __  |___ ___ ___ ___ ___| |_|_|_____ ___') +
-  chalk.green('\n | __ -| . |   |   | -_|  _|  _| |     | -_|') +
-  chalk.green('\n |_____|__,|_|_|_|_|___|_| |_| |_|_|_|_|___|\n');
+  chalk.green('\n  _____    _____                     _   _') +
+  chalk.green('\n | __  |  | __  |___ ___ ___ ___ ___| |_|_|_____ ___') +
+  chalk.green('\n | __ |   | __ -| . |   |   | -_|  _|  _| |     | -_|') +
+  chalk.green('\n |_|      |_____|__,|_|_|_|_|___|_| |_| |_|_|_|_|___|\n');
   this.log(message);
 };
